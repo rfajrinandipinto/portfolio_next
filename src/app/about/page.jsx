02b23fb8350b5534d714377565a6f3fef5751985
1706_ R Fajri Nandipinto <rfajrin@students.amikom.ac.id>
2023-04-10 20:29:51 +0700

    initial

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -13,29 +13,29 @@ const about = () => {
       </div>
       <div className=" text-lg flex flex-col gap-5 xl:pr-60 pr-0 text-gray-400">
         <p>
-          Hi there! My name is Rahamat Fajri Nandipinto, and I am an aspiring beginner full-stack developer with a passion for technology and an insatiable desire to learn. I am currently a college student, and I have been self-taught in
-          the field of web development.
+          Hi there! My name is Rahamat Fajri Nandipinto&lsquo; and I am an aspiring beginner full-stack developer with a passion for technology and an insatiable desire to learn&rsquo; I am currently a college student&lsquo; and I have been self-taught in
+          the field of web development&rsquo;
         </p>
-        <p>From an early age, I have always been fascinated by technology and how it can be used to solve complex problems. As a result, I began teaching myself how to code when I was in high school, and I quickly fell in love with it.</p>
+        <p>From an early age&lsquo; I have always been fascinated by technology and how it can be used to solve complex problems&rsquo; As a result&lsquo; I began teaching myself how to code when I was in high school&lsquo; and I quickly fell in love with it&rsquo;</p>
         <p>
-          One of my greatest strengths as a developer is my ability to adapt quickly to new technologies. I am always looking for new and innovative ways to solve problems and improve my skills. Whether it's learning a new framework or
-          programming language, I am always up for the challenge.
-        </p>
-        <p>
-          In addition to my technical skills, I am also an excellent communicator and public speaker. I believe that communication is key in any team setting, and I always strive to keep my teammates informed and engaged. I am also
-          comfortable presenting my ideas and solutions to large groups of people, which has proven to be a valuable asset in my previous team projects.
+          One of my greatest strengths as a developer is my ability to adapt quickly to new technologies&rsquo; I am always looking for new and innovative ways to solve problems and improve my skills&rsquo; Whether it's learning a new framework or
+          programming language&lsquo; I am always up for the challenge&rsquo;
         </p>
         <p>
-          Speaking of teamwork, I have several experiences working in a team environment, both in academic and professional settings. I understand the importance of collaboration, and I always strive to be a team player. I believe that the
-          best ideas come from a diverse group of individuals working together towards a common goal.
+          In addition to my technical skills&lsquo; I am also an excellent communicator and public speaker&rsquo; I believe that communication is key in any team setting&lsquo; and I always strive to keep my teammates informed and engaged&rsquo; I am also
+          comfortable presenting my ideas and solutions to large groups of people&lsquo; which has proven to be a valuable asset in my previous team projects&rsquo;
         </p>
         <p>
-          In terms of my technical skills, I am proficient in several frameworks and programming languages, including React.js, Next.js, Express.js, and Node.js. These tools have enabled me to create robust and dynamic web applications, and
-          I am always looking to expand my knowledge in this area.
+          Speaking of teamwork&lsquo; I have several experiences working in a team environment&lsquo; both in academic and professional settings&rsquo; I understand the importance of collaboration&lsquo; and I always strive to be a team player&rsquo; I believe that the
+          best ideas come from a diverse group of individuals working together towards a common goal&rsquo;
         </p>
         <p>
-          In summary, I am Rahamat Fajri Nandipinto, a college student, self-taught full-stack developer, quick to adapt to new technologies, with excellent communication and public speaking skills, and several experiences working in a
-          team. I am excited to continue learning and growing as a developer, and I can't wait to see where my skills and passion for technology will take me in the future!
+          In terms of my technical skills&lsquo; I am proficient in several frameworks and programming languages&lsquo; including React&rsquo;js&lsquo; Next&rsquo;js&lsquo; Express&rsquo;js&lsquo; and Node&rsquo;js&rsquo; These tools have enabled me to create robust and dynamic web applications&lsquo; and
+          I am always looking to expand my knowledge in this area&rsquo;
+        </p>
+        <p>
+          In summary&lsquo; I am Rahamat Fajri Nandipinto&lsquo; a college student&lsquo; self-taught full-stack developer&lsquo; quick to adapt to new technologies&lsquo; with excellent communication and public speaking skills&lsquo; and several experiences working in a
+          team&rsquo; I am excited to continue learning and growing as a developer&lsquo; and I can't wait to see where my skills and passion for technology will take me in the future!
         </p>
       </div>
     </div>
